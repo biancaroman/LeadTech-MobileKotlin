fbaad708efbc1407acd853755146d39ed36e352d
biancaroman <biancaroman1425@gmail.com>
2024-11-04 13:51:18 -0300

    ajustes na atualizacao de lookbooks

--- app/src/main/java/com/example/leadtech_mobile/activity/AddLookbookActivity.kt
+++ app/src/main/java/com/example/leadtech_mobile/activity/AddLookbookActivity.kt
@@ -1,5 +1,6 @@
 package com.example.leadtech_mobile.activity
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
@@ -35,6 +36,10 @@ class AddLookbookActivity : AppCompatActivity() {
                 val lookbook = Lookbook(id = generateId(), nome = nome, pecas = emptyList())
                 lookbookViewModel.adicionarLookbook(lookbook)
                 Toast.makeText(this, "Lookbook adicionado com sucesso!", Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+
                 finish()
             } else {
                 Toast.makeText(this, "Por favor, insira um nome para o lookbook.", Toast.LENGTH_LONG).show()
